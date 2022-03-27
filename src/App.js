d37390a5e622928cbcbb1dcd5df9b1cfb5b956c4
divya prakash <d.prakash1506@gmail.com>
2022-03-28 00:09:35 +0530

    updated the inline checks

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
   async function fetchMovieHandler() {
     setIsLoading(true);
     try {
-      const response = await fetch("https://swapi.dev/api/film");
-      if(!response.ok){
-        throw new Error('Oops! Something went wrong.')
+      const response = await fetch("https://swapi.dev/api/films");
+      if (!response.ok) {
+        throw new Error("Oops! Something went wrong.");
       }
       const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
@@ -28,7 +28,28 @@ function App() {
     } catch (error) {
       setError(error.message);
     }
+
     setIsLoading(false);
+  }
+  let content = (
+    <p>
+      No Movies found, try clicking on Fetch Movies{" "}
+      <span role="img" aria-label="arrow">
+        👆
+      </span>
+    </p>
+  );
+
+  if (movies.length >0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if(error){
+    content = <p>{error}</p>
   }
 
   return (
@@ -36,19 +57,7 @@ function App() {
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && !error && (
-          <p>
-            No Movies found, try clicking on Fetch Movies{" "}
-            <span role="img" aria-label="arrow">
-              👆
-            </span>
-          </p>
-        )}
-        {isLoading && <p>Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
