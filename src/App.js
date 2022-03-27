878a62c4fe773e29d0f4bec1df050499a3ea7b9e
divya prakash <d.prakash1506@gmail.com>
2022-03-27 23:54:57 +0530

    Added loading and error handling logics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]); //state for movies name
+  const [isLoading, setIsLoading] = useState(false); //state for loaing status
+  const [error, setError] = useState(null); //state for error
 
-  function fetchMovieHandler(){
-    fetch('https://swapi.dev/api/films')
-    .then((response) => 
-    {return response.json()})
-    .then((data) => {
+  async function fetchMovieHandler() {
+    setIsLoading(true);
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if(!response.ok){
+        throw new Error('Oops! Something went wrong.')
+      }
+      const data = await response.json();
       const transformedMovies = data.results.map((movieData) => {
-        return{
+        return {
           id: movieData.episode_id,
           title: movieData.title,
           openingText: movieData.openingCrawl,
-          releaseDate: movieData.release_date
+          releaseDate: movieData.release_date,
         };
       });
-      setMovies(transformedMovies)
-    })
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
   }
 
   return (
@@ -29,7 +37,17 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && (
+          <p>
+            No Movies found, try clicking on Fetch Movies{" "}
+            <span role="img" aria-label="arrow">
+              👆
+            </span>
+          </p>
+        )}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </React.Fragment>
   );
